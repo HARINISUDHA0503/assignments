946b3d651b08d12c079fd2a794c42dd3f9f105c6
HARINISUDHA0503 <115806019+HARINISUDHA0503@users.noreply.github.com>
2022-11-21 12:47:36 +0530

    Add files via upload

---
+++ Assignments6/assignments6/Assignments6Application.java
@@ -0,0 +1,13 @@
+package com.Assignments6.assignments6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignments6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignments6Application.class, args);
+	}
+
+}
